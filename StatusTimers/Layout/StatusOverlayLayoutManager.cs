0667859f7cb1991f56cff302f940a4118cad08d5
Zeffuro <Jeffroiscool@gmail.com>
2025-09-22 23:15:32 +0200

    KTK Update (parts rework)

--- StatusTimers/Layout/StatusOverlayLayoutManager.cs
+++ StatusTimers/Layout/StatusOverlayLayoutManager.cs
@@ -48,21 +48,23 @@ public class StatusOverlayLayoutManager<TKey>(
             LeftOffset = 21,
             RightOffset = 21
         };
-        _backgroundNode.AddPart(new Part {
-            TexturePath = "ui/uld/HUDLayout.tex",
-            Size = new Vector2(44, 32),
-            TextureCoordinates = new Vector2(0, 0)
-        });
-        _backgroundNode.AddPart(new Part {
-            TexturePath = "ui/uld/HUDLayout.tex",
-            Size = new Vector2(88, 16),
-            TextureCoordinates = new Vector2(0, 16)
-        });
-        _backgroundNode.AddPart(new Part {
-            TexturePath = "ui/uld/HUDLayout.tex",
-            Size = new Vector2(156, 80),
-            TextureCoordinates = new Vector2(0, 24)
-        });
+        _backgroundNode.AddPart(
+            new Part {
+                TexturePath = "ui/uld/HUDLayout.tex",
+                Size = new Vector2(44, 32),
+                TextureCoordinates = new Vector2(0, 0)
+            },
+            new Part {
+                TexturePath = "ui/uld/HUDLayout.tex",
+                Size = new Vector2(88, 16),
+                TextureCoordinates = new Vector2(0, 16)
+            },
+            new Part {
+                TexturePath = "ui/uld/HUDLayout.tex",
+                Size = new Vector2(156, 80),
+                TextureCoordinates = new Vector2(0, 24)
+            }
+        );
 
         GlobalServices.NativeController.AttachNode(_backgroundNode, ownerOverlay);
 
